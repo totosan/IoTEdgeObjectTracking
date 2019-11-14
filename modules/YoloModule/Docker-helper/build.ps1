cf14f7c9ecd788cfd6d6742798da53b9b846fb25
Thomas Tomow <toto_san@live.com>
2019-11-14 10:45:55 +0100

    removed seperate Darknet build for jetson

--- modules/YoloModule/Docker-helper/build.ps1
+++ modules/YoloModule/Docker-helper/build.ps1
@@ -1,12 +1,10 @@
-# builds are for (arm64v8 & amd64)
-# iot sdk builder
+
+# iot sdk builder (arm64v8 / amd64)
 docker build -f "./Dockerfile.iot-sdk-python-builder.amd64" -t "iotcontainertt.azurecr.io/iot-sdk-python-builder:amd64" .
 if($?){docker push iotcontainertt.azurecr.io/iot-sdk-python-builder:amd64}
 if($?){docker build -f "./Dockerfile.iot-sdk-python-builder.arm64v8" -t "iotcontainertt.azurecr.io/iot-sdk-python-builder:arm64v8" .}
 if($?){docker push iotcontainertt.azurecr.io/iot-sdk-python-builder:arm64v8}
 
-# darknet builder
-if($?){docker build -f "./Dockerfile.Darknet-cpu.amd64" -t "iotcontainertt.azurecr.io/darknet-python:amd64" .}
-if($?){docker push iotcontainertt.azurecr.io/iotcontainertt.azurecr.io/darknet-python:amd64}
-if($?){docker build -f "./Dockerfile.Darknet-cpu.arm64v8" -t "iotcontainertt.azurecr.io/darknet-python:arm64v8" .}
-if($?){docker push iotcontainertt.azurecr.io/iotcontainertt.azurecr.io/darknet-python:arm64v8}
+# darknet builder (amd64)
+if($?){docker build -f "./Dockerfile.Darknet.amd64" -t "iotcontainertt.azurecr.io/darknet-python:amd64" .}
+if($?){docker push iotcontainertt.azurecr.io/darknet-python:amd64}
