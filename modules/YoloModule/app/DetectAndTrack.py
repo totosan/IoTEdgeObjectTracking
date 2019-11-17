8e250c6dcf2befc74feba606949a638d8f81b43a
Thomas Tomow <toto_san@live.com>
2019-11-17 17:22:16 +0100

    fixed usb sound change on port

--- modules/YoloModule/app/DetectAndTrack.py
+++ modules/YoloModule/app/DetectAndTrack.py
@@ -196,8 +196,6 @@ class DetectAndTrack():
                 details = self.__getObjectDetails__(frame,rect)
                 if details and len(details)>0:
                     predictions = details["predictions"]
-                    for item in predictions:
-                        print(f'{item["tagName"]} ({item["probability"]})')
                     isPost = next((match for match in predictions if float(match["probability"])>0.8 and match["tagName"] == "Post"),None)
                     if isPost:
                         className = "post car"
